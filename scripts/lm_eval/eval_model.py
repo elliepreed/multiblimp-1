5cf1b39c3a90ded47cec87c33a0bdba9bb6fb479
elliepreed <166074435+elliepreed@users.noreply.github.com>
2025-07-24 16:35:37 +0100

    Update eval_model.py

--- scripts/lm_eval/eval_model.py
+++ scripts/lm_eval/eval_model.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src/lm_eval")))
-import sys
 import argparse
 from glob import glob
 import torch  
